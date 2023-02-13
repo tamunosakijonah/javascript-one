445e749ad679f032837ad87be42607f55ae3b182
tamunosaki jonah <tamunosakijonah@gmail.com>
2023-02-13 21:24:21 +0000

    new one

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function multFun(a,b){
+    console.log(a*b)
+}
+multFun(7,6)
+
+let firstName = 'Tamunosaki'
+let surName = 'Jonah'
+let stutternTrack = 'frontEnd'
+let skills = 'Html,css,javascript'
+let myColors = 'black,white'
+let country = '1960'
+
+console.log('my name is ' + firstName +' ' + surName + 
+' .'+ 'i am in' + ' '+ stutternTrack + ' ' + 'track' + 
+',i am proficient in' + ' '+ skills +' ' + ' .'+ 
+'my favourite colors are' + ' ' + myColors + ' '
++ 'my country gained independence in' + ' ' + country )
